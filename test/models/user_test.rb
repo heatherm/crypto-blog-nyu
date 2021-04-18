6f275c4e4c5aef0c0cb05d3ec45eeddcdafbc795
Heather Moore <heatherm@users.noreply.github.com>
2021-04-18 14:26:21 +0200

    backfill user test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should save a valid user" do
+    user = User.new(email: "foo@example.com", password: "password")
+    assert user.save
+  end
 end
