5f74617eafda1a13ad95931db24155da93fb1303
Heather Moore <heatherm@users.noreply.github.com>
2021-05-01 14:26:40 +0200

    update authy credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :authy_authenticatable, :authy_lockable, :database_authenticatable, :lockable,
    :registerable, :recoverable, :rememberable, :validatable
-  # devise :authy_authenticatable, :authy_lockable, :database_authenticatable, :lockable         
 end
