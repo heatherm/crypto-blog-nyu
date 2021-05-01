a5fb599610f2a53d82b26dd9439d709c1725ef6b
Heather Moore <heatherm@users.noreply.github.com>
2021-05-01 11:13:08 +0200

    add hsts headers on responses

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,7 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
+  config.ssl_options = { hsts: { expires: 1.year, subdomains: false } }
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
